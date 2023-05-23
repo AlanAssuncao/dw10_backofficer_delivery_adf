67d901b1c006b3a92d2d076470d4f26264e4d616
AlanAssuncao <alansoaresassuncao@gmail.com>
2023-05-22 22:24:06 -0300

    dia 5 concluido

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -2,6 +2,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import './modules/home/home_page.dart';
 import 'modules/core/core_module.dart';
 import 'modules/login/login_module.dart';
+import 'modules/order/order_module.dart';
 import 'modules/payment_type/payment_type_module.dart';
 import 'modules/products/products_module.dart';
 import 'modules/template/base_layout.dart';
@@ -30,6 +31,10 @@ class AppModule extends Module {
               '/products',
               module: ProductsModule(),
             ),
+            ModuleRoute(
+              '/order',
+              module: OrderModule(),
+            )
           ],
         ),
       ];
